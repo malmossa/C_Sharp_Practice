2c049a006a364f22d307459f9d106ca03a285c43
Mansor Almossa <malmossa@gmail.com>
2023-07-14 09:23:39 -0700

    completed

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -5,30 +5,9 @@ namespace Practice
   {
     static void Main(string[] args)
     {
-            // Declaring jagged array with items
-            int[][] jaggedArray =
-            {
-                new int[]{1, 2},
-                new int[]{3, 4, 5},
-                new int[]{6},
-                new int[]{7, 8}
-            };
+            string firstName = "mansor";
 
-            // Access an item in the jagged array
-            int[] secondArray = jaggedArray[1]; // [3, 4, 5]
-    
-            // Iterating through the array 
-            foreach (int[] array in jaggedArray)
-            {
-                foreach (int item in array)
-                {
-                    Console.Write(item);
-                    Console.Write(" ");
-                }
-
-                Console.WriteLine();
-            }
-
+            Console.WriteLine($"The length {firstName} is {firstName.Length}");
 
 
             Console.ReadLine();
