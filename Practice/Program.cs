ae928cd2d8ff7d079cfdb1eb23eb1911f1916df7
Mansor Almossa <malmossa@gmail.com>
2023-07-16 13:06:03 -0700

    completed

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -5,19 +5,28 @@ namespace Practice
   {
     static void Main(string[] args)
     {
-            
-           void PrintOnTheConsole(string message = "Hey", int times = 3)
-            {
-                for (int i = 0; i < times; i++)
-                {
-                    Console.WriteLine(message);
-                }
-            }
+            double total1 = Checkout(3.99, 5.75, 15);
+            double total2 = Checkout(3.99, 5.75);
+            double total3 = Checkout(3.99, 5.75, 1.99, 100);
 
-            PrintOnTheConsole();
+            Console.WriteLine(total1);
+            Console.WriteLine(total2);
+            Console.WriteLine(total3);
 
             Console.ReadLine();
 
+    }
+
+    static double Checkout(params double[] prices)
+    {
+            double total = 0;
+
+            foreach (double price in prices)
+            {
+                total += price;
+            }
+
+            return total;
     }
   }
  
