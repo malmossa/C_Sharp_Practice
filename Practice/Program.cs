4cc5244f22121684b1a16f38bc5870f48e718957
Mansor Almossa <malmossa@gmail.com>
2023-08-09 09:15:51 -0700

    completed

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -5,10 +5,31 @@ namespace Practice
   {
     static void Main(string[] args)
     {
-            Console.WriteLine("Hi, what is your name? ");
-            string name = Console.ReadLine();
+            // this is a statement
+            Console.WriteLine("Hello, World!"); 
 
-            Console.WriteLine($"Hello {name}.");
+            // this is a multiple statements
+            Console.WriteLine("My name is mansor.");
+            Console.WriteLine("I love to code.");
+
+            // Whitespace
+            Console.WriteLine
+                ("Hello, World!"            );
+
+            // this is an expression
+            Console.WriteLine("Hi, " + "my name is mansor");
+
+            // variable
+            string name; // declaring
+            name = "mansor"; // assignment
+            Console.WriteLine("Hi " + name);
+
+            // reading text from the console
+            string myName;
+            Console.WriteLine("What is your name?");
+            myName = Console.ReadLine();
+            Console.WriteLine("Hi " + myName);
+
 
 
 
