6d4d4d34d0ecdb0be378b25fc71a78bc5a4871e4
Mansor Almossa <malmossa@gmail.com>
2023-08-25 10:11:35 -0700

    completed

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -7,14 +7,22 @@ namespace Practice
     {
         static void Main(string[] arge)
         {
-            bool canIvote = true;
+            /* Homework: Create a Console Application 
+               that has variables to hold a person's name, age,
+               if they are alive, and their phone number. */
 
+            string name = "Mansor";
+            string phoneNumber = "62662942255";
+            int age = 50;
+            bool isAlive = true;
 
-            Console.WriteLine(canIvote);
-
+            Console.WriteLine($"Name: {name}.");
+            Console.WriteLine($"Age: {age}.");
+            Console.WriteLine($"Phone: {phoneNumber}.");
+            Console.WriteLine($"Is he alive: {isAlive}.");
 
 
             Console.ReadLine();
         }
     }
-}
+    }
