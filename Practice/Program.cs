fff6a232f80487d4437a0342dab98138ec307719
Mansor Almossa <malmossa@gmail.com>
2023-08-27 09:34:28 -0700

    completed

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -7,27 +7,20 @@ namespace Practice
     {
         static void Main(string[] arge)
         {
-            Console.Write("What is your first name? ");
-            string firstName = Console.ReadLine();
+            Console.Write("What is your age? ");
+            string ageText = Console.ReadLine();
 
-            switch(firstName.ToLower())
+            bool isValidAge = int.TryParse(ageText, out int age);
+
+            if(isValidAge == true)
             {
-                case "mansor":
-                    Console.WriteLine("Hello, Mansor");
-                    break;
-                case "nujood":
-                    Console.WriteLine("Hello, Nujood");
-                    break;
-                default:
-                    Console.WriteLine("Hello, there!");
-                    break;
+                age += 10;
+                Console.WriteLine($"You are going to be {age} years old in 10 years. ");
+            } else
+            {
+                Console.WriteLine("That was not a valid age!");
             }
 
-
-
-
-
-            
             Console.ReadLine();
         }
     }
