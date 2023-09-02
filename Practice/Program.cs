805b45d2740f55d59af1890b416ab48fc020bdee
Mansor Almossa <malmossa@gmail.com>
2023-09-01 19:45:20 -0700

    completed

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -6,49 +6,51 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            double myNum = 4.123456789012345; 
-            Console.WriteLine(myNum);
+            //bool boolFromString = bool.Parse("true");
+            //Console.WriteLine($"Data type: {boolFromString.GetType()}");
 
-            // Boolean, true or false
-            bool canIvote = true;
+            //int intFromString = int.Parse("100");
+            //Console.WriteLine($"Data type: {intFromString.GetType()}");
 
-            // String
+            //double doubleFromString = double.Parse("1.234");
+            //Console.WriteLine($"Data type: {doubleFromString.GetType()}");
 
+            //string stringValue = doubleFromString.ToString();
+            //Console.WriteLine($"Data type: {stringValue.GetType()}");
 
-            // Integer, whole number
-            Console.WriteLine($"Integer range from: {int.MinValue} To {int.MaxValue}");
-                
-            // Long 
-            Console.WriteLine($"Long range from: {long.MinValue} To {long.MaxValue}");
+            ///* Explicit convertion, becuse we are going to lose some data
+            // * like converting double to interger, we lose all decimal values*/
 
-            // Decimal are more precision
-            Console.WriteLine($"Decimal range from: {decimal.MinValue} To {decimal.MaxValue}");
+            //double doubleNumber = 12.345;
+            //Console.WriteLine($"Integer: {(int)doubleNumber}");
 
-            // Double, floating point number
-            Console.WriteLine($"Double range from: {double.MinValue} To {double.MaxValue}");
+            ///* Implicit convertion, when the smallest type is been converted
+            // * into a larger type, so no data is been lost */
+            //int intNumber = 10;
+            //Console.WriteLine($"Double: {(double)intNumber}");
 
-            // Float
-            Console.WriteLine($"Float range from: {float.MinValue} To {float.MaxValue}");
+            int myInt = 10;
+            double myDouble = 5.25;
+            bool myBool = true;
 
-
-
-
-            /* Decimal is more precision, 28 digits after the period.
-               use commonly with money, add "M" at the end */
-            decimal decimalNumber = 3.1234567890123456789012345678M; 
-
-            /* Double is the most common use, precision 15 digits after the period*/
-            double doubleNumber = 3.144780093260476;
-
-            /* Float precision is 7 digits after the period, add "F" at the end*/
-            float floatNumber = 3.14478009326047620529F;
+            Console.WriteLine(Convert.ToString(myInt)); // convert int to string
+            Console.WriteLine(Convert.ToDouble(myInt)); // convert int to double
+            Console.WriteLine(Convert.ToInt32(myDouble)); // convert double to int
+            Console.WriteLine(Convert.ToString(myBool)); // convert bool to string
 
 
 
-            Console.WriteLine(canIvote);
-            Console.WriteLine(decimalNumber);
-            Console.WriteLine(doubleNumber);
-            Console.WriteLine(floatNumber);
+
+
+
+
+
+
+
+
+
+
+
 
 
             Console.ReadLine();
