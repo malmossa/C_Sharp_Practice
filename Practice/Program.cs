b276708420cc5e43500a8d7bf027cbd64edb12e4
Mansor Almossa <malmossa@gmail.com>
2023-09-05 08:33:47 -0700

    completed

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -8,9 +8,26 @@ namespace Practice
         static void Main(string[] args)
         {
 
+            int[] numbers = {1, 2, 3, 4, 5};
+
+            for (int i = 0; i < numbers.Length; i++) 
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            
 
 
-            Console.WriteLine("Hello mansor almossa");
+
+
+
+
+
+
+
+
+
+
 
 
 
