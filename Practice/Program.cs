98e087f0941cb09b0c4a37482dce92724aed7ae9
Mansor Almossa <malmossa@gmail.com>
2023-10-08 10:15:36 -0700

    completed

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -5,29 +5,29 @@ namespace Practice
     internal class Program
     {
         static void Main(string[] args)
-        {
-            int number1 = 5;
-            int number2 = 5;
+        {        
+           
+            string fullName = "Mansor Almossa";
 
-            // unary +
-            Console.WriteLine(+number1);
-
-            // unary -
-            Console.WriteLine(-number1);
-
-            // increment ++ by one
-            Console.WriteLine(++number2);
-
-            // derement -- by one 
-            Console.WriteLine(--number2);
+            Console.WriteLine($"Hello Mr. {fullName.Substring(7, 7)}");
 
 
 
-            
+
+
+
+
+
+
+
+
+
+
+
+
 
 
             Console.ReadLine();
-
         }
     }
 }
