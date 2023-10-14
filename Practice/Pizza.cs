bdf500d9163469932a8f9269a5a40a1ecee1b6af
Mansor Almossa <malmossa@gmail.com>
2023-10-14 11:34:48 -0700

    completed

---
+++ Practice/Pizza.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Practice
+{
+    internal class Pizza
+    {
+        string bread;
+        string sauce;
+        string cheese;
+        string topping;
+
+        internal Pizza (string bread, string sauce, string cheese, string topping)
+        {
+            this.bread = bread;
+            this.sauce = sauce;
+            this.cheese = cheese;
+            this.topping = topping;
+        }
+
+        internal Pizza(string bread, string sauce, string cheese)
+        {
+            this.bread = bread;
+            this.sauce = sauce;
+            this.cheese = cheese;         
+        }
+    }
+}
