2fc48a5d8fb46fd4abb9c59fe359dd6ac223365c
Mansor Almossa <malmossa@gmail.com>
2023-10-13 23:48:10 -0700

    completed

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -9,12 +10,20 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Hello mansor almossa");
+            bigList();
+                       
             Console.ReadLine();
         }
+       
+        static void bigList()
+        {
+            List<string> names = Enumerable.Repeat("mansor", 10).ToList();
 
-        
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
            
     }
 
