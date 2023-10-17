030b520b5184107d1312316a4766b687cde1950c
Mansor Almossa <malmossa@gmail.com>
2023-10-16 19:53:30 -0700

    completed

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -7,13 +7,41 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            
+            Car car = new Car();
+            Bicycle bicycle = new Bicycle();
+
+            Vehicle[] vehicles = { car, bicycle};
+
+            foreach (Vehicle vehicle in vehicles)
+            {
+                vehicle.Go();
+            }
 
             Console.ReadLine();
 
         }
 
-        
+        class Vehicle
+        {
+            internal virtual void Go() { }
+        }
+
+        class Car : Vehicle 
+        {
+            internal override void Go()
+            {
+                Console.WriteLine("The car is moving!");
+            }
+
+        }
+
+        class Bicycle : Vehicle 
+        {
+            internal override void Go()
+            {
+                Console.WriteLine("The bicycle is moving!");
+            }
+        }
                  
     }
 
