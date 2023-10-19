257b8a712c8dd778a8359246d28e3795e2778bd6
Mansor Almossa <malmossa@gmail.com>
2023-10-19 09:38:07 -0700

    completed

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -6,7 +6,16 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            string name = "mansor";
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            // Step 1: Deferred execution
+            var evenNumbers = numbers.Where(n =>
+            {
+                Console.WriteLine($"Evaluating if {n} is even");
+                return n % 2 == 0;
+            });
+
+
 
 
             Console.ReadLine();
