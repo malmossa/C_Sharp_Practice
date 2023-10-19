a0e0f6a89c4c72082bbb256b1f1b91bb482da235
Mansor Almossa <malmossa@gmail.com>
2023-10-19 08:24:15 -0700

    completed

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,43 +1,18 @@
 ﻿using System;
-using System.Threading;
 
 namespace Practice
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-                    
+            string name = "mansor";
 
-            
-
-           
 
             Console.ReadLine();
         }
-
-
-        class Rectangle
-        {
-            private float _width;
-            private float _height;
-           
-            public Rectangle(float width, float height)
-            {
-                _width = width;
-                _height = height;             
-            }
-
-            public float Width
-            {
-                get { return _width;}
-                set { _width = value; }
-            }
-        }
-              
     }
 }
-
 
 
 
