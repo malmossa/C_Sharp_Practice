af7a62e3e9a61f6216e7a32025f40c0e860efaea
Mansor Almossa <malmossa@gmail.com>
2023-10-21 21:46:44 -0700

    completed

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -6,19 +6,40 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 1, 2, 3, 4, 5 };
-
-            // Step 1: Deferred execution
-            var evenNumbers = numbers.Where(n =>
-            {
-                Console.WriteLine($"Evaluating if {n} is even");
-                return n % 2 == 0;
-            });
-
-
-
-
+            Rectangle rectangle1 = new Rectangle(5, 10);
+           
+            Console.WriteLine($"Width is {rectangle1.Width}");
+            Console.WriteLine($"Height is {rectangle1.Height}");
+            Console.WriteLine($"Area is {rectangle1.CalculateArea()}");
+            Console.WriteLine($"Circumference is {rectangle1.CalculateCircumference()}");
+           
             Console.ReadLine();
+        }
+
+    }
+
+    class Rectangle
+    {
+        // fields
+        public int Width;
+        public int Height;
+
+        // constructor
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        // methods
+        public int CalculateCircumference()
+        {
+            return 2 * Width + 2 * Height;
+        }
+
+        public int CalculateArea()
+        {
+            return Width * Height;
         }
     }
 }
