1068cbd484d88ad4927211f1df2fbb7fa50760f0
Mansor Almossa <malmossa@gmail.com>
2023-11-01 09:04:50 -0700

    commit

---
+++ Practice/Employee.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace Practice
+{
+    // Class declaration and naming
+    public class Employee
+    {
+        // Constructor
+        public Employee() 
+        {
+            // Initialization code gose here.
+        }
+
+        // Fields
+        private string firstName;
+        private int age;
+
+        // Setters and Getters
+        public string FirstName 
+        { 
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        // Method
+        public void PerformWork()
+        {
+            // method imlementation gose here.
+        }
+    }
+}
--- Practice/Program.cs
+++ Practice/Program.cs
@@ -6,37 +6,12 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            ChestState currentState = ChestState.Locked;
 
-            while (true)
-            {
-                Console.Write($"The chest is {currentState}. What do you want to do? ");
-                string userInput = Console.ReadLine();
+            Console.WriteLine("Hello World!");
 
-                if(currentState == ChestState.Locked && userInput == "unlock") 
-                {
-                    currentState = ChestState.Closed;
-                }
-
-                if(currentState == ChestState.Closed && userInput == "open")
-                {
-                    currentState = ChestState.Open;
-                }
-
-                if(currentState == ChestState.Open && userInput == "close") 
-                {
-                    currentState = ChestState.Closed;
-                }
-
-                if(currentState == ChestState.Closed && userInput == "lock")
-                {
-                    currentState = ChestState.Locked;
-                }
-            }
-           
         }
 
-        enum ChestState { Open, Closed, Locked }
+        
 
     }
 
