dee3afd4820992e14e3ad78172312af596a72bf6
Mansor Almossa <malmossa@gmail.com>
2023-12-13 09:35:44 -0800

    completed

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      /* The code below
+       will print the worlds Hello World to the screen*/
+      Console.WriteLine("Hello World!");
+
+
+
+    }
+  }
+}
